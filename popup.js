cb4191330d424d35f54893b98c1a6cdec25e5068
Jeremy Sao <khmersao1@gmail.com>
2018-02-01 19:36:30 -0500

    HTML now populated using Javascript

--- popup.js
+++ popup.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var url = "https://api.coinmarketcap.com/v1/ticker/ethereum/"
-
-  function myFunction(){
+  var url = "https://api.coinmarketcap.com/v1/ticker/"
+  function function1(x){
       let request = new XMLHttpRequest();
       request.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
           var obj = JSON.parse(this.responseText);
-          document.getElementById("price").innerHTML = obj[0].symbol+': '+ obj[0].price_usd;
-          document.getElementById("change").innerHTML = "24Hr Change:" + "(" + obj[0].percent_change_24h + "%)";
-          console.log(obj);
+
+          for(var i=0; i<5; i++){
+            var row = document.getElementsByClassName("row")[i];
+            var price = document.createElement("h3");
+            var change = document.createElement("h3");
+            price.innerHTML = obj[i].symbol+': $'+ obj[i].price_usd;
+            change.innerHTML ="24Hr Change:" + "(" + obj[i].percent_change_24h + "%)";
+            row.appendChild(price);
+            row.appendChild(change);
+            console.log(obj);
+          }
         }
       }
-      request.open("GET",url,true);
+      request.open("GET",x,true);
       request.send();
   }
-  myFunction();
+  function1(url);
 })
