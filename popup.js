70f4bf6eb063003826f22044b92754e0873b53fc
Jeremy Sao <khmersao1@gmail.com>
2018-02-03 18:00:35 -0500

    added new fonts, started variable coin selector

--- popup.js
+++ popup.js
@@ -5,16 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
       request.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
           var obj = JSON.parse(this.responseText);
-
+          var coins = [0,1,2,5,10];
           for(var i=0; i<5; i++){
             var row = document.getElementsByClassName("row")[i];
             var symbol = document.createElement("h2");
             var price = document.createElement("h3");
             var change = document.createElement("h3");
-            symbol.innerHTML = obj[i].symbol + " ("+ obj[i].name+")";
-            price.innerHTML ='$'+ obj[i].price_usd;
-            change.innerHTML ="(" + obj[i].percent_change_24h + "%)";
-            if(obj[i].percent_change_24h > 1){
+            symbol.innerHTML = obj[coins[i]].symbol + " ("+ obj[coins[i]].name+")";
+            price.innerHTML ='$'+ obj[coins[i]].price_usd;
+            change.innerHTML ="(" + obj[coins[i]].percent_change_24h + "%)";
+            if(obj[coins[i]].percent_change_24h > 1){
               change.style.backgroundColor="green";
             }else{
               change.style.backgroundColor="red";
